9b099abbb2b0aa7b19cdd5fca6941ef49c80f3f8
Jane Kelly <jane.kelly@gmail.com>
2019-12-29 14:07:49 +0000

    Updates

---
+++ precache-manifest.1393963dc3a0cc2166751dbba3fa43a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ffcda52ca4ce2ed2cbd203756355f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "56a47bf71294ee5b4741",
+    "url": "/monsters-rolodex/static/css/main.ffff7edf.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56a47bf71294ee5b4741",
+    "url": "/monsters-rolodex/static/js/main.9b473e16.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
